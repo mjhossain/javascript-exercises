b67e4d4cd9ed4968dd6c4c3a9a852d3b4cae84f0
mjhossain <mjhossainnyc@gmail.com>
2022-11-28 23:13:15 -0500

    Exercise 1 to 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(usrArr, ...elem) {
+        return usrArr.filter(x => !elem.includes(x))  
 };
 
 // Do not edit below this line
