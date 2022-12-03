42c96e1f0f046aa46220cef64c5d08a8f0861c48
mjhossain <mjhossainnyc@gmail.com>
2022-12-03 14:48:32 -0500

    Ex 7 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  if(x.length != 0) {
+    const total = x.reduce((t,n) => {
+      return t + n;
+    })
+    return total;
+  } else {
+    return 0;
+  }
+  
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  if(x.length != 0) {}
+  const total = x.reduce((t,n) => {
+    return t * n;
+  })
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(x === 0) {
+    return 1;
+  } else {
+    return x * factorial(x-1);
+  }
 };
 
 // Do not edit below this line
