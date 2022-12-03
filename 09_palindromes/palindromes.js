b837ffcb101754c945864929cdd6391bccc5d856
mjhossain <mjhossainnyc@gmail.com>
2022-12-03 15:13:13 -0500

    Ex 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const wordTest = word.split('');
+    // console.log('Word:', word, 'Reverse Word:', wordTest.reverse().join(''));
+    if(word.replace(/[^a-zA-Z]/g, "").toLowerCase() == word.replace(/[^a-zA-z]/g, "").toLowerCase().split('').reverse().join("")){
+        return true;
+    } else {
+        return false;
+    }  
 };
 
 // Do not edit below this line
