b7c51cd08455f0de61d9be15f80a060a036a9aca
mjhossain <mjhossainnyc@gmail.com>
2022-12-05 18:41:41 -0500

    Ex 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return oldestPerson = people.reduce((oldest, currentPerson) => {
+        const oldAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+        return oldAge < currentAge ? currentPerson : oldest;
+    })
 };
+
+const getAge = function(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
