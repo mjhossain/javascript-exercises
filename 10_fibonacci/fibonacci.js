a6e46e56d2dbb97e8c1afcf99c56694b7f9ecc02
mjhossain <mjhossainnyc@gmail.com>
2022-12-05 11:06:09 -0500

    Ex 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if(n<0) return "OOPS";
+
+    let fib = new Array(n+2)
+    let i;
+
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for(i=2; i <= n; i++) {
+        fib[i] = fib[i-1] + fib[i-2]
+    }
+    // console.log(fib)
+    return fib[n];
 };
 
 // Do not edit below this line
